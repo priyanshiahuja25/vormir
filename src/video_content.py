bb7fc7bd908003c66c2a6875271a8b3e86095ae7
Uday Sharma <itsudayy@gmail.com>
2023-02-10 04:51:54 +0530

    Creating a video that is actually useful

--- src/video_content.py
+++ src/video_content.py
@@ -1,4 +1,5 @@
 from .comment import Comment
+from bs4 import BeautifulSoup
 from html2image import Html2Image
 import os
 from gtts import gTTS, gTTSError
@@ -29,12 +30,16 @@ class VideoContent:
         hti = Html2Image(output_path=path, custom_flags=['--virtual-time-budget=1000', '--hide-scrollbars'])
         hti.size = (500, 350)
         try:
+            template_file = os.path.join(os.getcwd(), 'assets', 'reddit_comment.html')
             for comment in self.comments:
+                VideoContent.change_html(file_name=template_file, text=comment.body)
                 hti.screenshot(html_file='assets/reddit_comment.html', css_file='assets/reddit_comment.css',
                                save_as=f'{comment.comment_id}.png')
 
             # TITLE SCREENSHOT
             hti.size = (550, 400)
+            template_file = os.path.join(os.getcwd(), 'assets', 'reddit_title.html')
+            VideoContent.change_html(file_name=template_file, text=self.title)
             hti.screenshot(html_file='assets/reddit_title.html', css_file='assets/reddit_title.css',
                            save_as='title.png')
         except Exception as e:
@@ -66,19 +71,37 @@ class VideoContent:
             comment_audio = AudioFileClip(filename=os.path.join(self.path, 'audio', f'{comment.comment_id}.mp3'),
                                           fps=44100)
             clips.append(ImageClip(os.path.join(self.path, 'images', f'{comment.comment_id}.png'))
-                         .set_duration(t=comment_audio.duration).set_audio(comment_audio))
+                         .set_duration(t=comment_audio.duration + 1).set_audio(comment_audio))
 
         # Make title clip
         comment_audio = AudioFileClip(filename=os.path.join(self.path, 'audio', f'title.mp3'),
                                       fps=44100)
         clips.insert(0,
-                     ImageClip(os.path.join(self.path, 'images', f'title.png')).set_duration(t=comment_audio.duration+2)
+                     ImageClip(os.path.join(self.path, 'images', f'title.png')).set_duration(
+                         t=comment_audio.duration + 1)
                      .set_audio(comment_audio))
 
         # Make the actual Video
         # print(clips)
         concat_clip = concatenate_videoclips(clips, method="compose")
         concat_clip.write_videofile(os.path.join(self.path, "main.mp4"), fps=24)
+
+    @staticmethod
+    def change_html(file_name, text):
+        """I will make this method more reliable in the future maybe"""
+        # Load the HTML file
+        with open(file_name) as f:
+            soup = BeautifulSoup(f, "html.parser")
+
+        # Find the specific tag you want to change
+        tag = soup.find("p", {"class": "content"})
+
+        # Change the text of the tag
+        tag.string = text
+
+        # Save the modified HTML file
+        with open(file_name, "w") as f:
+            f.write(str(soup))
 
     @staticmethod
     def make_folder(submission_id):
